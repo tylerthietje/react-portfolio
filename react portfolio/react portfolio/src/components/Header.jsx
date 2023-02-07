9248160f06355542a0006c393686b3222df72d30
Tyler Thietje <tylerthietje@gmail.com>
2023-02-07 17:45:16 -0600

    added header section

--- react portfolio/react portfolio/src/components/Header.jsx
+++ react portfolio/react portfolio/src/components/Header.jsx
@@ -1,9 +1,25 @@
+
+
+
 const Header = () => {
     return (
-        <div className="header">
-            
-        </div>
+      <div>
+        <section className="hero-image">
+          <div className="hero-text">
+            <h1 className="title">Hi, I'm Tyler Thietje</h1>
+            <p className="profile">
+              I’m a Front End Developer specializing in responsive, accessible
+              and performant user experiences with a strong background in
+              project management.
+            </p>
+            <div className="btns">
+              <button className="cta-btn">Contact</button>
+              <button className="resume">View my resume</button>
+            </div>
+          </div>
+        </section>
+      </div>
     );
 }
- 
+
 export default Header;
