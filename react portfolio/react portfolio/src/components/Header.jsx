8832e498ef913ba224d87acb64dcd1b0c413b241
Tyler Thietje <tylerthietje@gmail.com>
2023-02-08 15:04:57 -0600

    added hero cta button styling

--- react portfolio/react portfolio/src/components/Header.jsx
+++ react portfolio/react portfolio/src/components/Header.jsx
@@ -1,24 +1,24 @@
-
+import Navbar from './Navbar';
 
 const Header = () => {
-    return (
-      <div>
-        <section className="hero-image">
-          <div className="hero-text">
-            <h1 className="title">Hi, I'm Tyler Thietje</h1>
-            <p className="profile">
-              I’m a Front End Developer specializing in responsive, accessible
-              and performant user experiences with a strong background in
-              project management.
-            </p>
-            <div className="btns">
-              <button className="cta-btn">Contact</button>
-              <button className="resume">View my resume</button>
-            </div>
+  return (
+    <div>
+      <section className="hero-image">
+        <div className="hero-text">
+          <h1 className="title">Hi, I'm Tyler Thietje</h1>
+          <p className="profile">
+            I’m a Front End Developer specializing in responsive, accessible and
+            performant user experiences with a strong background in project
+            management.
+          </p>
+          <div className="btns">
+            <button className="contact">Contact</button>
+            <button className="resume">View my resume</button>
           </div>
-        </section>
-      </div>
-    );
-}
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Header;
