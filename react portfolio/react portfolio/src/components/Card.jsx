fe3f72dced9be9a22f9f4ba87b540b98564a38d0
Tyler Thietje <tylerthietje@gmail.com>
2023-03-05 11:52:01 -0600

    Added Card component and used it to render project cards

---
+++ react portfolio/react portfolio/src/components/Card.jsx
@@ -0,0 +1,24 @@
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <img
+          src="../assets/images/guess-the-word.png"
+          alt="Project"
+          className="card-image"
+        />
+        <h1 className="card-title">Project 1</h1>
+        <h2 className="card-subtitle">Skills Used?</h2>
+        <p className="card-desc">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. At beatae,
+          nostrum non explicabo nam eaque, in cum ipsam dolor, sed perferendis
+          pariatur voluptatem saepe iusto assumenda maiores repellat similique
+          laborum?
+        </p>
+      </div>
+      ;
+    </div>
+  );
+};
+
+export default Card;
--- react portfolio/react portfolio/src/components/Projects.jsx
+++ react portfolio/react portfolio/src/components/Projects.jsx
@@ -1,79 +1,16 @@
+import '../assets/images/guess-the-word.png';
+import Card from './Card';
+
 const Projects = () => {
   return (
     <div className="projects">
       <a id="projects"></a>
       <h1 className="projects-heading">&lt;My Projects /&gt;</h1>
       <div className="cards">
-        <div className="card">
-          <img
-            src="#"
-            alt="Project"
-            className="card-image"
-          />
-          <h1 className="card-title">Project 1</h1>
-          <h2 className="card-subtitle">Skills Used?</h2>
-          <p className="card-desc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. At beatae,
-            nostrum non explicabo nam eaque, in cum ipsam dolor, sed perferendis
-            pariatur voluptatem saepe iusto assumenda maiores repellat similique
-            laborum?
-          </p>
-        </div>
-        <div className="card">
-          <img src="#" alt="Project" className="card-image" />
-          <h1 className="card-title">Project 1</h1>
-          <h2 className="card-subtitle">Skills Used?</h2>
-          <p className="card-desc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. At beatae,
-            nostrum non explicabo nam eaque, in cum ipsam dolor, sed perferendis
-            pariatur voluptatem saepe iusto assumenda maiores repellat similique
-            laborum?
-          </p>
-        </div>
-        <div className="card">
-          <img src="#" alt="Project" className="card-image" />
-          <h1 className="card-title">Project 1</h1>
-          <h2 className="card-subtitle">Skills Used?</h2>
-          <p className="card-desc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. At beatae,
-            nostrum non explicabo nam eaque, in cum ipsam dolor, sed perferendis
-            pariatur voluptatem saepe iusto assumenda maiores repellat similique
-            laborum?
-          </p>
-        </div>
-        <div className="card">
-          <img src="#" alt="Project" className="card-image" />
-          <h1 className="card-title">Project 1</h1>
-          <h2 className="card-subtitle">Skills Used?</h2>
-          <p className="card-desc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. At beatae,
-            nostrum non explicabo nam eaque, in cum ipsam dolor, sed perferendis
-            pariatur voluptatem saepe iusto assumenda maiores repellat similique
-            laborum?
-          </p>
-        </div>
-        <div className="card">
-          <img src="#" alt="Project" className="card-image" />
-          <h1 className="card-title">Project 1</h1>
-          <h2 className="card-subtitle">Skills Used?</h2>
-          <p className="card-desc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. At beatae,
-            nostrum non explicabo nam eaque, in cum ipsam dolor, sed perferendis
-            pariatur voluptatem saepe iusto assumenda maiores repellat similique
-            laborum?
-          </p>
-        </div>
-        <div className="card">
-          <img src="#" alt="Project" className="card-image" />
-          <h1 className="card-title">Project 1</h1>
-          <h2 className="card-subtitle">Skills Used?</h2>
-          <p className="card-desc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. At beatae,
-            nostrum non explicabo nam eaque, in cum ipsam dolor, sed perferendis
-            pariatur voluptatem saepe iusto assumenda maiores repellat similique
-            laborum?
-          </p>
-        </div>
+        <Card />
+        <Card />
+        <Card />
+        
       </div>
     </div>
   );
